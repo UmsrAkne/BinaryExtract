5762ab4849f269d6cb98edc35119963cfb5858f9
Akn <sibata486@gmail.com>
2021-03-07 22:14:59 +0900

    init

---
+++ BinaryExtract/Models/DropBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace BinaryExtract.Models {
+    class DropBehavior : Behavior<Window> {
+
+        protected override void OnAttached() {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            this.AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            this.AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e) {
+            // ファイルパスの一覧の配列
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            System.Diagnostics.Debug.WriteLine(files[0]);
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+        protected override void OnDetaching() {
+            base.OnDetaching();
+            this.AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            this.AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+    }
+}
