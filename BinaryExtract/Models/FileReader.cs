f540fc54bba45d4f81061df940945a58e30652b3
Akn <sibata486@gmail.com>
2021-03-08 23:51:01 +0900

    バイナリファイルを読み込むモデルクラスを追加

---
+++ BinaryExtract/Models/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExtract.Models {
+    class FileReader {
+
+        public FileInfo TargetFileInfo{ get; private set; }
+        private FileStream fileStream;
+
+        public FileReader(FileInfo targetFileInfo) {
+            this.TargetFileInfo = targetFileInfo;
+            fileStream = new FileStream(TargetFileInfo.FullName, FileMode.Open, FileAccess.Read);
+        }
+
+    }
+}
