fbf3ce1ba3b8610e513d82a6f19c3f47e59bcfb2
Akn <sibata486@gmail.com>
2021-03-09 00:36:48 +0900

    機能追加 / バイト配列内から指定のパターンを検索するメソッドを追加

---
+++ BinaryExtractTests/Models/FileReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryExtract.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryExtract.Models.Tests {
+    [TestClass()]
+    public class FileReaderTests {
+        [TestMethod()]
+        public void searchTest() {
+            var dummyFile = new FileInfo("dummyText.txt");
+            if (!dummyFile.Exists) {
+                using (StreamWriter sw = dummyFile.CreateText()) {
+                    sw.WriteLine("This is dummy text");
+                }
+            }
+
+            var fileReader = new FileReader(new FileInfo("dummyText.txt"));
+            fileReader.search(null);
+            fileReader.search(new List<Byte>(new Byte[] { 115, 32, 105 }));
+        }
+    }
+}
