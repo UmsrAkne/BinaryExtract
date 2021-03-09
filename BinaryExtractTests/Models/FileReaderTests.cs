ea866a9dd538a211208be6a6c1c972656a158aae
Akn <sibata486@gmail.com>
2021-03-09 15:54:39 +0900

    search() のテストを追記

--- BinaryExtractTests/Models/FileReaderTests.cs
+++ BinaryExtractTests/Models/FileReaderTests.cs
@@ -20,8 +20,28 @@ namespace BinaryExtract.Models.Tests {
             }
 
             var fileReader = new FileReader(new FileInfo("dummyText.txt"));
-            fileReader.search(null);
-            fileReader.search(new List<Byte>(new Byte[] { 115, 32, 105 }));
+
+            var nullError = false;
+            try {
+                fileReader.search(null);
+            }
+            catch (ArgumentException) {
+                nullError = true;
+            }
+
+            var zeroLengthError = false;
+            try {
+                fileReader.search(new List<Byte>());
+            }
+            catch (ArgumentException) {
+                zeroLengthError = true;
+            }
+
+            Assert.IsTrue(nullError);
+            Assert.IsTrue(zeroLengthError);
+
+            List<long> result = fileReader.search(new List<Byte>(new Byte[] { 115, 32, 105 }));
+            Assert.AreEqual(result.First(), 3);
         }
     }
 }
