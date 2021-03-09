0db6fd966a518b6d21ec4d3d83ee3222dcbe6e9d
Akn <sibata486@gmail.com>
2021-03-09 23:23:14 +0900

    １６進数から１０進数への変換機能をもったクラスを追加

---
+++ BinaryExtract/Models/HexConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExtract.Models {
+    public class HexConverter {
+        public List<Byte> convertHexToDecimals(string hex) {
+            var hexStrings = hex.Split(' ');
+            var decimals = new List<Byte>();
+
+            for(int i = 0; i < hexStrings.Length; i++) {
+                decimals.Add(Convert.ToByte(hexStrings[i], 16));
+            }
+
+            return decimals;
+        }
+    }
+}
---
+++ BinaryExtractTests/Models/HexConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryExtract.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExtract.Models.Tests {
+    [TestClass()]
+    public class HexConverterTests {
+        [TestMethod()]
+        public void convertHexToDecimalsTest() {
+            HexConverter hexConverter = new HexConverter();
+            var list = hexConverter.convertHexToDecimals("00 0a ff");
+
+            Assert.AreEqual(list[0], 0);
+            Assert.AreEqual(list[1], 10);
+            Assert.AreEqual(list[2], 255);
+        }
+    }
+}
